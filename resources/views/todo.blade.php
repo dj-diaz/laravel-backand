5b13721f9878263f6bad861c57787bdd35da97ca
dj-diaz <denniel.joshua@gmail.com>
2016-08-13 19:48:24 +0800

    fix readme

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts')
 
 @section('content')
+
     <div class="container">
         <hr>
         <div class="col-lg-12 table-responsive container">
-            <a class="btn btn-default" href=""><span class="glyphicon glyphicon-plus"></span> Add</a>
+            <button class="btn btn-default" href="" id="addModal" data-toggle="modal" data-target="#myModal">
+                <span class="glyphicon glyphicon-plus"></span> Add
+            </button>
             <hr>
             <table class="table table-hover table-bordered">
                 <thead>
@@ -36,5 +39,33 @@
             
         </div>
     </div>
-   
+
+    <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+        
+    
 @stop
+
+@section('custom-js')
+    <script>
+    $('#addModal').on('shown.bs.modal', function () {
+        $('#myModal').focus()
+     })
+     </script>
+@endsection
